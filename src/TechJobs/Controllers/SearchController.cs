6b5a0a34fd97bad7418b094bcb874b033727ee14
Mia Anderson <meicy101@gmail.com>
2020-01-08 12:41:18 -0600

    Finished

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -13,6 +13,25 @@ namespace TechJobs.Controllers
             return View();
         }
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            if (searchType.Equals("all"))
+            {
+
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+            return View("Index");
+            }
+
+            else
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                return View("Index");
+            }
+
+        }
+
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
